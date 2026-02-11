88bd9d0a0555a5184426614450f06cc143b07854
truongnam <tgknam2005@gmail.com>
2026-02-11 23:50:16 +0700

    feat: first commit

---
+++ src/main/java/io/github/dontknowhatodo/DontknowhatodoApplication.java
@@ -0,0 +1,13 @@
+package io.github.dontknowhatodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DontknowhatodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DontknowhatodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dontknowhatodo/DontknowhatodoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dontknowhatodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DontknowhatodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
