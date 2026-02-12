fd17feb3d8be882a20c32bf96ab4cb5f8fbaaee5
truongnam <tgknam2005@gmail.com>
2026-02-12 15:37:13 +0700

    feat: graphql exception handling

---
+++ src/main/java/io/github/dontknowhatodo/exception/GraphQLExceptionHandler.java
@@ -0,0 +1,21 @@
+package io.github.dontknowhatodo.exception;
+
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.stereotype.Component;
+
+import graphql.GraphQLError;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+
+@Component
+public class GraphQLExceptionHandler extends DataFetcherExceptionResolverAdapter {
+
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+        return GraphqlErrorBuilder.newError()
+                .message(ex.getMessage())
+                .path(env.getExecutionStepInfo().getPath())
+                .errorType(graphql.ErrorType.DataFetchingException)
+                .build();
+    }
+}
