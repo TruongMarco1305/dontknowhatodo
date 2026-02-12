e944f7d9e1ec9aaf7538cb0d56f7f63912836806
truongnam <tgknam2005@gmail.com>
2026-02-12 15:18:12 +0700

    feat: init config

---
+++ src/main/java/io/github/dontknowhatodo/config/JpaConfig.java
@@ -0,0 +1,10 @@
+// Entity base config (timestamps, ...)
+package io.github.dontknowhatodo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
